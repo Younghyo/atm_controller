96843bb43f27239d16e7393d7cb7b11060892730
younghyo <sonata8756@gmail.com>
2022-08-25 16:42:58 +0900

    project init

---
+++ src/AtmControllerSolution/AtmController/AtmController.cpp
@@ -0,0 +1,30 @@
+// AtmController.cpp : Defines the exported functions for the DLL.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "AtmController.h"
+#include <iostream>
+
+using namespace std;
+
+
+// This is an example of an exported variable
+ATMCONTROLLER_API int nAtmController=0;
+
+// This is an example of an exported function.
+ATMCONTROLLER_API int fnAtmController(void)
+{
+    return 0;
+}
+
+// This is the constructor of a class that has been exported.
+CAtmController::CAtmController()
+{
+    return;
+}
+
+void CAtmController::foo()
+{
+    cout << "foo" << endl;
+}
---
+++ src/AtmControllerSolution/AtmController/AtmController.h
@@ -0,0 +1,23 @@
+// The following ifdef block is the standard way of creating macros which make exporting
+// from a DLL simpler. All files within this DLL are compiled with the ATMCONTROLLER_EXPORTS
+// symbol defined on the command line. This symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file see
+// ATMCONTROLLER_API functions as being imported from a DLL, whereas this DLL sees symbols
+// defined with this macro as being exported.
+#ifdef ATMCONTROLLER_EXPORTS
+#define ATMCONTROLLER_API __declspec(dllexport)
+#else
+#define ATMCONTROLLER_API __declspec(dllimport)
+#endif
+
+// This class is exported from the dll
+class ATMCONTROLLER_API CAtmController {
+public:
+	CAtmController(void);
+	// TODO: add your methods here.
+	void foo();
+};
+
+extern ATMCONTROLLER_API int nAtmController;
+
+ATMCONTROLLER_API int fnAtmController(void);
